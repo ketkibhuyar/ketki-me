7d65471d7b83627247980e5ef5c6ca6e99cf4c82
d5_ketki_69775 <bhuyarketki261@gmail.com>
2023-05-06 22:59:42 +0530

    refactor(footer): update resume link

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -22,7 +22,7 @@ const Footer = () => {
       Created using react 💖
     </p>
     <div className='footer-download-resume'>
-      <button className='download-button' onClick={() => openLink('https://drive.google.com/file/d/1_NcQKm5HtsvF59JTjkVj2JkG8PWj5SdR/view?usp=share_link')}>
+      <button className='download-button' onClick={() => openLink('https://drive.google.com/file/d/123-CuF75Jr4XD7TZKfNtoN1rKNK3FuzI/view?usp=share_link')}>
         Resume
       </button>
     </div>
