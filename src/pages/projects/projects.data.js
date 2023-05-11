3e0d09775d452094f2456a48ceb343b4c0ca0b7c
d5_ketki_69775 <bhuyarketki261@gmail.com>
2023-05-11 23:00:47 +0530

    feat(projects): add angular project

--- src/pages/projects/projects.data.js
+++ src/pages/projects/projects.data.js
@@ -6,14 +6,25 @@ export const ProjectData = [
       'A Car rental Website where user can register, do login, select a car which he want to rent and make payment, and see order details',
     frontend: ['React','Javascript'],
     backend: ['Spring boot','Java'],
-    database: 'MySQL'
+    database: 'MySQL',
   },
   {
     id: 2,
     name: 'ketki-bhuyar',
     description:
       'My portfolio website',
-    frontend: ['React','Javascript']
+    frontend: ['React','Javascript'],
+    websiteLink: 'https://ketkibhuyar.github.io/ketki-me/',
+    githubLink: '//github.com/ketkibhuyar/ketki-me'
+  },
+  {
+    id: 3,
+    name: 'ketki',
+    description:
+      'My Angular portfolio website',
+    frontend: ['Angular','Typescript'],
+    websiteLink: 'https://ketkibhuyar.github.io/ketki/',
+    githubLink: 'https://github.com/ketkibhuyar/ketki'
   }
 ]
 
--- src/pages/projects/projects.page.js
+++ src/pages/projects/projects.page.js
@@ -1,29 +1,49 @@
 import Layout from '../../components/layout/layout.component'
 import './projects.scss'
 import ProjectData from '../projects/projects.data'
+import githubImage from '../../assets/github.svg'
+import linkImage from '../../assets/link.avif'
 
 const ProjectPage = () => {
+  const openLink = (link) => {
+    window.open(link, '_blank');
+  }
+
   return (
     <Layout title="Projects">
       <div className="projects-container">
-        {ProjectData.map(({ id, name, description, frontend, backend }) => (
+        {ProjectData.map(({ id, name, description, frontend, backend, githubLink, websiteLink }) => (
           <div key={id} className="project-card">
             <div className="project-basic-details">
               <p className="project-name">{name}</p>
               <p className="project-description">{description}</p>
             </div>
-            <div className="project-tech-container">
-            <div className='tech-wrapper'>
-              {frontend && <p>Frontend - </p>}
-                {frontend?.map((technology,index) => (
-                  <><p>{technology}</p>{index!== frontend.length-1 && <span>,</span>}</>
-                ))}
-              </div>
-              <div className='tech-wrapper'>
-              {backend && <p>Backend - </p>}
-                {backend?.map((technology,index) => (
-                  <><p>{technology}</p>{index!== backend.length-1 && <span>,</span>}</>
-                ))}
+            <div>
+              <div className="project-tech-container">
+
+                <div className='tech-wrapper'>
+                  {frontend && <p>Frontend - </p>}
+                  {frontend?.map((technology, index) => (
+                    <><p>{technology}</p>{index !== frontend.length - 1 && <span>,</span>}</>
+                  ))}
+                </div>
+                <div className='tech-wrapper'>
+                  {backend && <p>Backend - </p>}
+                  {backend?.map((technology, index) => (
+                    <><p>{technology}</p>{index !== backend.length - 1 && <span>,</span>}</>
+                  ))}
+                </div>
+                <div class="social-wrapper">
+                  {websiteLink && <span onClick={() => openLink({ websiteLink })} >
+                    <img class="social-image" src={linkImage} alt=''></img>
+                  </span>
+                  }
+
+                  {githubLink && <span onClick={() => openLink({ githubLink })} href=''>
+                    <img class="social-image" src={githubImage} alt=''
+                    ></img>
+                  </span> }
+                </div>
               </div>
             </div>
           </div>
